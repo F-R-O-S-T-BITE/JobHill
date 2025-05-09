5965712660a723f02a740569aa9c2551d9e6a801
santiagosauma <113849899+santiagosauma@users.noreply.github.com>
2025-05-08 20:50:23 -0600

    Fix: Isaac Errors 🪓

--- jobhill/src/app/register/page.tsx
+++ jobhill/src/app/register/page.tsx
@@ -109,15 +109,15 @@ export default function Register() {
                             id="termsCheckbox"
                             checked={formData.termsAccepted}
                             onChange={handleChange}
-                            className="h-4 w-4 text-[#0353A4] border-gray-300 rounded"
+                            className="h-4 w-4 text-[#0353A4] border-gray-300 rounded cursor-pointer"
                         />
                         <label htmlFor="termsCheckbox" className="ml-2 text-[12px] font-poppins text-black">
-                            I agree to the <span className="text-[#0353A4]">terms & policy</span>
+                            I agree to the <span className="text-[#0353A4] hover:underline cursor-pointer">terms & policy</span>
                         </label>
                     </div>
                     <button
                         type="submit"
-                        className="w-full bg-[#0353A4] text-white py-2 rounded font-poppins font-medium"
+                        className="w-full bg-[#0353A4] text-white py-2 rounded font-poppins font-medium cursor-pointer hover:bg-[#034383] transition-colors"
                     >
                         Signup
                     </button>
@@ -126,15 +126,15 @@ export default function Register() {
                 <button 
                 onClick={handleGoogleSignIn}
                 disabled={isLoading}
-                className="w-full flex items-center justify-center border border-gray-300 py-2 rounded font-poppins mb-3 text-black">
+                className="w-full flex items-center justify-center border border-gray-300 py-2 rounded font-poppins mb-3 text-black hover:bg-gray-100 transition-colors cursor-pointer">
                     <FaGoogle className="text-red-500 mr-2" />
                     Sign in with Google
                 </button>
                 <button 
                 onClick={handleGithubSignIn}
                 disabled={isLoading}
-                className="w-full flex items-center justify-center border border-gray-300 py-2 rounded font-poppins text-black">
-                    <FaGithub className="mr-2" />
+                className="w-full flex items-center justify-center border border-gray-300 py-2 rounded font-poppins text-black hover:bg-gray-100 transition-colors cursor-pointer">
+                    <FaGithub className="text-red-500 mr-2" />
                     Sign in with Github
                 </button>
                 <div className="mt-6 text-center text-[14px] font-poppins text-black">
